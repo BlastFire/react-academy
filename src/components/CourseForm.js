617208a5185ed14a0fc98d55c18e5dba206f3260
Velizar Atanasov <vatanasov@sbnd.int>
2017-09-19 18:13:34 +0300

    super basic form fields

---
+++ src/components/CourseForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => {
+    return (
+        <div>
+            <label>
+                {label}
+            </label>
+            <div>
+                <input {...input} placeholder={label} type={type} />
+                {touched &&
+                    error &&
+                    <span>
+                        {error}
+                    </span>}
+            </div>
+        </div>
+    )
+}
+
+let CourseForm = props => {
+
+    const { handleSubmit } = props
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <Field name="name" type="text" component={renderField} />
+                <label htmlFor="category">Category</label>
+                <Field name="category" type="text" component={renderField} />
+                <label htmlFor="teacherName">Teacher's name</label>
+                <Field name="teacherName" type="text" component={renderField} />
+                <label htmlFor="teacherEmail">Teacher's email</label>
+                <Field name="teacherEmail" type="text" component={renderField} />
+                <label htmlFor="description">Description</label>
+                <Field name="description" component="textarea" />
+                <label htmlFor="language">Language</label>
+                <div>
+                    <Field name="language" component="select">
+                        <option />
+                        <option value="BG">Bulgarian</option>
+                        <option value="GB">English</option>
+                        <option value="FR">French</option>
+                    </Field>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+// CourseForm = connect(
+//     state => state
+// )(CourseForm)
+
+CourseForm = reduxForm({
+    form: 'courseForm'
+})(CourseForm)
+
+export default CourseForm
--- src/store.js
+++ src/store.js
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 //import { reactReduxFirebase, firebaseStateReducer } from 'react-redux-firebase'
+import testReducer from './reducers/testreducer'
+
+
+const reducer = combineReducers({
+    testReducer,
+    form: formReducer
+})
 
 export default createStore(reducer,
     composeWithDevTools(applyMiddleware(thunk))
