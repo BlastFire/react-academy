bc14f75a0e4ed4b26ba1fa91670ee79e84c4bed5
defaxed <defaxed@gmail.com>
2017-12-02 13:44:11 +0200

    hoc simplified

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -8,11 +8,6 @@ import CourseForm from './CourseForm'
 import CourseFormEdit from './CourseFormEdit'
 import CourseDescription from './CourseDescription'
 import Formichka from './Formichka'
-import { compose } from 'redux';
-import { Field, reduxForm } from 'redux-form'
-import { connect } from 'react-redux'
-import { fetchConfigLanguages, addCourse } from '../reducers/courseReducer'
-import { withRouter } from 'react-router-dom'
 
 const Courses = ({ match }) => {
 
@@ -25,11 +20,7 @@ const Courses = ({ match }) => {
                     </Col>
                     <Col xs="9">
                         <Switch>
-                            {/* <Route exact path="/courses/add" component={CourseForm} /> */}
-                            <Route exact path="/courses/add" component={enhance(CourseForm)} />
-                            {/* <Route exact path="/courses/add" render={ props => {
-                                return enhance(Formichka)
-                            }} /> */}
+                            <Route exact path="/courses/add" component={Formichka} />
                             <Route exact path={`${match.url}/:courseId/edit`} component={CourseFormEdit} />
                             <Route path={`${match.url}/:courseId`} render={({ match }) => (
                                 <DisplayCourse courseId={match.params.courseId} />
@@ -42,23 +33,5 @@ const Courses = ({ match }) => {
         </div>
     )
 }
-
-
-// const enhance = (Formichka) => props => {
-//     return (<Formichka {...props} />)
-// }
-
-const enhance = compose(
-    connect(
-        state => ({ languageConfig: state.crs.configCourse.languages }),
-        { fetchConfigLanguages, addCourse }
-    ),
-    withRouter,
-    reduxForm({
-        form: 'courseForm'
-    })
-)
-
-
 
 export default Courses
--- src/components/Formichka.js
+++ src/components/Formichka.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { compose } from 'redux';
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { fetchConfigLanguages, addCourse } from '../reducers/courseReducer'
+import { withRouter } from 'react-router-dom'
+import CourseForm from './CourseForm'
 
-const Formichka = props => {
-    return (<h1>formichka</h1>)
-}
+
+const Formichka = props => (
+    <div>
+        <X />
+        <Y />
+    </div>
+)
+
+// const withAddForm = compose(
+//     connect(
+//         state => ({ languageConfig: state.crs.configCourse.languages }),
+//         { fetchConfigLanguages, addCourse }
+//     ),
+//     withRouter,
+//     reduxForm({
+//         form: 'courseForm'
+//     })
+// )
+
+// const enhance = CourseFormHoc => props => (CourseFormHoc)
+// const Form = enhance(withAddForm(CourseForm))
+
+const X = props => (<h1>{props.name}</h1>)
+
+const hoc = overrides => Comp => props => (<Comp {...props} {...overrides} />)
+
+const Y = hoc({name: "Pietro"})(X)
+
+
 
 export default Formichka
