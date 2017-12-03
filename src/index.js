6325f58a2f9a10fc1eb80a65c0835afd3a4b4ba9
BlastFire <defaxed@gmail.com>
2017-12-03 20:03:08 +0200

    epics initial

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 
 ReactDOM.render(
